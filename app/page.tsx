fee410793477f11b6cf32c414081011e4e510fd4
Nicholas Culpin <njculpin@gmail.com>
2024-02-15 15:54:15 -0800

    next init.

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="max-w-5xl w-full justify-start font-mono text-sm lg:flex">
+        <h1 className="text-white">Mt Aptos</h1>
+      </div>
+    </main>
+  );
+}
